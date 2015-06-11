55db1fbbcb0466b90d7741ad4161a788fc820e52
diwayou <diwayou@qq.com>
2015-06-11 23:02:11 +0800

    Initial commit

---
+++ src/main/java/com/diwayou/App.java
@@ -0,0 +1,13 @@
+package com.diwayou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
