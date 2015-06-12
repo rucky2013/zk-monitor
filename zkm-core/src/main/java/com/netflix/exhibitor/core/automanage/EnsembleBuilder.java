802af8512247fb6f4be98aa6425d250688d33242
cn40387 <diwayou@qq.com>
2015-06-12 18:02:29 +0800

    remove exhibitor

--- zkm-core/src/main/java/com/netflix/exhibitor/core/automanage/EnsembleBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.netflix.exhibitor.core.automanage;
-
-import com.netflix.exhibitor.core.state.ServerList;
-
-interface EnsembleBuilder {
-    public boolean newEnsembleNeeded();
-
-    public ServerList createPotentialServerList();
-}
