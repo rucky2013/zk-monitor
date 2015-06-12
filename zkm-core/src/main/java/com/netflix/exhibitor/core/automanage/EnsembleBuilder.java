ca47b356d4f96a572cb8689df1eba2b06760acec
cn40387 <diwayou@qq.com>
2015-06-12 17:29:33 +0800

    remote persist

---
+++ zkm-core/src/main/java/com/netflix/exhibitor/core/automanage/EnsembleBuilder.java
@@ -0,0 +1,9 @@
+package com.netflix.exhibitor.core.automanage;
+
+import com.netflix.exhibitor.core.state.ServerList;
+
+interface EnsembleBuilder {
+    public boolean newEnsembleNeeded();
+
+    public ServerList createPotentialServerList();
+}
